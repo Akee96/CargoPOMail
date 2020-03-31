2d9b360183ffacae8014e8a8fc4b0ac39bc863e9
Nayon Fonseka <nayonfonsekanf@gmail.com>
2020-03-31 14:36:47 +0530

    UI Design - PO Mail

---
+++ CargoPOMail/Controllers/TransactionController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CargoPOMail.Controllers
+{
+    public class TransactionController : Controller
+    {
+        // GET: Transaction
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Imports()
+        {
+            return View();
+        }
+        public ActionResult Exports()
+        {
+            return View();
+        }
+    }
+}
